4c2459777d2430aa6f7239fbe1f5fb10fcc0e9c7
DritiS <118894837+DritiS@users.noreply.github.com>
2024-01-22 21:48:42 +0530

    done

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function calculateBMI() {
+  // Move these lines inside the function
+  let height = document.getElementById('Height').value;
+  let weight = document.getElementById('Weight').value;
+
+  // Check if height and weight are non-empty and numeric
+  if (height !== '' && weight !== '' && !isNaN(height) && !isNaN(weight)) {
+      let bmi = (Number(weight) * 10000) / (Number(height) * Number(height));
+      document.getElementById("demo").innerHTML = "Your BMI is: " + bmi.toFixed(2);
+  } else {
+      document.getElementById("demo").innerHTML = "Please enter valid height and weight.";
+  }
+
+  return false;
+
+}
+
+function changeImage() {
+  let image = document.getElementById('myImage'); // Declare image variable here
+  let height = document.getElementById('Height').value;
+  let weight = document.getElementById('Weight').value;
+
+  let bmi = (Number(weight) * 10000) / (Number(height) * Number(height));
+
+  
+  if (bmi <= 18.5) {
+    image.src = "images/Underweight (2).png";
+  } else if (bmi > 18.5 && bmi < 25) {
+    image.src = "images/Normal.png";
+  } else if (bmi >= 25 && bmi < 30) {
+    image.src = "images/Overweight.png";
+  } else {
+    image.src = "images/Obese.png";
+  }
+}
+
+function calculateBMI2() {
+  // Move these lines inside the function
+  let height = document.getElementById('Height').value;
+  let weight = document.getElementById('Weight').value;
+
+  // Check if height and weight are non-empty and numeric
+  if (height !== '' && weight !== '' && !isNaN(height) && !isNaN(weight)) {
+      let bmi2 = (Number(weight) * 703) / (Number(height) * Number(height)*144);
+      document.getElementById("demo").innerHTML = "Your BMI is: " + bmi2.toFixed(2);
+  } else {
+      document.getElementById("demo").innerHTML = "Please enter valid height and weight.";
+  }
+
+  return false;
+}
+
+function changeImage2() {
+  let image = document.getElementById('myImage'); // Declare image variable here
+  let height = document.getElementById('Height').value;
+  let weight = document.getElementById('Weight').value;
+
+  let bmi2 = (Number(weight) * 703) / (Number(height) * Number(height)*144);
+
+  
+  if (bmi2 <= 18.5) {
+    image.src = "images/Underweight (2).png";
+  } else if (bmi2 > 18.5 && bmi2 < 25) {
+    image.src = "images/Normal.png";
+  } else if (bmi2 >= 25 && bmi2 < 30) {
+    image.src = "images/Overweight.png";
+  } else {
+    image.src = "images/Obese.png";
+  }
+}
+
+
